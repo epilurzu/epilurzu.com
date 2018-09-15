26eaa9312ecacb86f9fe7ee155881aa03d756c2c
Enrico <e.ipodda@googlemail.com>
2018-09-15 17:40:12 +0200

    fix display

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 http.createServer(function (req, res) {
-    fs.readFile('home.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
+    var request = url.parse(req.url, true);
+    var action = request.pathname;
+
+        if (action == '/images/502.svg') {
+            fs.readFile('home.html', function(err, data) {
+                var img = fs.readFileSync('./images/502.svg');
+                res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+                res.end(img, 'binary');
+            });
+        }
+        else{
+            fs.readFile('home.html', function(err, data) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            });
+        }
+
 }).listen(8080);
